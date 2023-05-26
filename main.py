6769ec8a1bd085caef3f86e98aa6bd5e50a097f7
OwenTalmo <oetalmage16@gmail.com>
2023-05-26 13:16:14 -0400

    Hand landmarks overlaid on video feed

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import time
 import cv2
 import numpy as np
 import mediapipe as mp
+# for visualizing results
+from mediapipe import solutions
+from mediapipe.framework.formats import landmark_pb2
+
+result_landmarks = mp.tasks.vision.HandLandmarkerResult
 
 def createLandmarker():
    # landmarker object
@@ -11,9 +16,14 @@ def createLandmarker():
    # callback function options
    # print, from https://developers.google.com/mediapipe/solutions/vision/hand_landmarker/python#live-stream
    def print_result(result: mp.tasks.vision.HandLandmarkerResult, output_image: mp.Image, timestamp_ms: int):
-      print('hand landmarker result: {}'.format(result.handedness))
-   # next is to add a callback that'll draw on the video stream
+      print('hand landmarker result: {}'.format(result))
+      global result_landmarks
+      result_landmarks = result
 
+   # next is to add a callback that'll draw on the video stream
+   # def update_result(result: mp.tasks.vision.HandLandmarkerResult):
+   #    result_landmarks = result
+   
    # options for running it
    options = mp.tasks.vision.HandLandmarkerOptions( 
       base_options = mp.tasks.BaseOptions(model_asset_path="hand_landmarker.task"), # path to model
@@ -27,13 +37,55 @@ def createLandmarker():
    # create and return landmarker object
    return landmarker.create_from_options(options)
 
+def draw_landmarks_on_image(rgb_image, detection_result: mp.tasks.vision.HandLandmarkerResult):
+   """Courtesy of https://github.com/googlesamples/mediapipe/blob/main/examples/hand_landmarker/python/hand_landmarker.ipynb"""
+   try:
+      if detection_result.hand_landmarks == []:
+         return rgb_image
+      else:
+         hand_landmarks_list = detection_result.hand_landmarks
+         handedness_list = detection_result.handedness
+         annotated_image = np.copy(rgb_image)
+
+         # Loop through the detected hands to visualize.
+         for idx in range(len(hand_landmarks_list)):
+            hand_landmarks = hand_landmarks_list[idx]
+            handedness = handedness_list[idx]
+
+            # Draw the hand landmarks.
+            hand_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
+            hand_landmarks_proto.landmark.extend([
+               landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in hand_landmarks
+            ])
+            solutions.drawing_utils.draw_landmarks(
+               annotated_image,
+               hand_landmarks_proto,
+               solutions.hands.HAND_CONNECTIONS,
+               solutions.drawing_styles.get_default_hand_landmarks_style(),
+               solutions.drawing_styles.get_default_hand_connections_style())
+
+            # Get the top left corner of the detected hand's bounding box.
+            height, width, _ = annotated_image.shape
+            x_coordinates = [landmark.x for landmark in hand_landmarks]
+            y_coordinates = [landmark.y for landmark in hand_landmarks]
+            text_x = int(min(x_coordinates) * width)
+            text_y = int(min(y_coordinates) * height) - 10
+
+            # Draw handedness (left or right hand) on the image.
+            cv2.putText(annotated_image, f"{handedness[0].category_name}",
+                        (text_x, text_y), cv2.FONT_HERSHEY_DUPLEX,
+                        2, (0,0,255), 3, cv2.LINE_AA)
+
+         return annotated_image
+   except:
+      return rgb_image
+
 def main():
    # access webcam
    cap = cv2.VideoCapture(0)
 
    # create landmarker
    hand_landmarker = createLandmarker()
-
 
    # open landmarker and stream data
    with hand_landmarker as landmarker:
@@ -44,6 +96,8 @@ def main():
          mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
          # process image
          landmarker.detect_async(image = mp_image, timestamp_ms = int(time.time() * 1000))
+         # draw landmarks on frame
+         frame = draw_landmarks_on_image(frame,result_landmarks)
 
          cv2.imshow('frame',frame)
          if cv2.waitKey(1) == ord('q'):
